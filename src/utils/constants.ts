84a1b10611f6b13c4e1719ede75b8e6d897ead04
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-03-03 23:39:47 +0100

    feat(database): create setting model

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const DB_CONNECTION_SUCCESS = 'Connected to Mongo successfully!';
 
-export const TWIITTER_APP_INIT_SUCCESS = 'Twitter client for app initialized successfully!';
-
 export const TWEET_PREFIX_KEY = 'caparledev_tweet_';
 
 export const RATE_LIMIT_CODE = 88;
+
+export const TEMPORARY_OAUTH_TOKEN = 'temporaryOauthToken';
