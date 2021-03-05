22a4fe471f6d86084ef442029ebfb324a86f44c4
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-03-05 15:35:23 +0100

    enh(bot): verify if a retweet can be performed because of rate limit

--- src/utils/retweetJob.ts
+++ src/utils/retweetJob.ts
@@ -1,18 +1,46 @@
 import cron from 'node-cron';
 
 import { CRON_RETWEET_INTERVAL } from '../config/env';
+import { Setting } from '../models/setting.model';
+import { RATE_LIMIT_TIME_SETTING_KEY } from './constants';
+
+const canPerformRetweet = async () => {
+  const rateLimitExpireSetting = await Setting.findOne({ key: RATE_LIMIT_TIME_SETTING_KEY });
+
+  if (!rateLimitExpireSetting || !rateLimitExpireSetting?.value) {
+    return true;
+  }
+
+  const rateLimitExpireTimestamp = parseInt(rateLimitExpireSetting.value, 10);
+  const isExpired = new Date().getTime() > rateLimitExpireTimestamp;
+
+  if (isExpired) {
+    await Setting.updateOne({ key: RATE_LIMIT_TIME_SETTING_KEY }, { value: null });
+
+    return true;
+  }
+
+  return false;
+};
 
 const startRetweetJob = () => {
   let isRunning = false;
 
   cron.schedule(
     CRON_RETWEET_INTERVAL,
-    () => {
+    async () => {
       if (isRunning) {
         return;
       }
       isRunning = true;
-      // TODO Logic here
+
+      const canRetweet = await canPerformRetweet();
+
+      if (!canRetweet) {
+        return;
+      }
+
+      // TODO Search tweet not retweeted in DB and retweet them
     },
     {
       scheduled: true,
